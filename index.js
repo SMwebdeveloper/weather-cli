d5c2c34694834990cd1962df350dfb566f9510d4
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2025-03-04 13:10:14 +0500

    add log service and packages

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const getArgs = require("./helpers/args")
-
+import getArgs from "./helpers/args.js"
+import { printSuccess, printError, printHelp } from "./services/log.service.js"
 const startCli = () => {
     const args = getArgs(process.argv)
     console.log(args)
+   
+
+    if(args.h) {
+        printHelp()
+    }
 }
 
 startCli()
