6617fe5d81bd0136f40ad9f6ae6bab7f7ed3da9c
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2025-03-16 18:07:32 +0500

    add storage service

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-import getArgs from "./helpers/args.js"
-import { printSuccess, printError, printHelp } from "./services/log.service.js"
+import getArgs from "./helpers/args.js";
+import { printSuccess, printError, printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token was saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 const startCli = () => {
-    const args = getArgs(process.argv)
-    console.log(args)
-   
+  const args = getArgs(process.argv);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+  }
+  if (args.t) {
+   return saveToken(args.t);
+  }
+};
 
-    if(args.h) {
-        printHelp()
-    }
-}
-
-startCli()
+startCli();
