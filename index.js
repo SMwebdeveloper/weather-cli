39d5812a9e03027da54c2b2e55bb14f572c74b8c
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2025-03-17 14:13:16 +0500

    save city function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import getArgs from "./helpers/args.js";
 import { getWeather } from "./services/api.service.js";
 import { printSuccess, printError, printHelp } from "./services/log.service.js";
-import { TOKEN_DICTIONARY, saveKeyValue } from "./services/storage.service.js";
+import { TOKEN_DICTIONARY, getKeyValue, saveKeyValue } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
   if (!token) {
@@ -16,32 +16,45 @@ const saveToken = async (token) => {
   }
 };
 
-const getForcast = async () => {
-  try {    
-   const response = await getWeather(process.env.CITY ?? "Uzbekistan");
-   console.log(response)
+const saveCity = async (city) => {
+  if (!city) {
+    printError("Don't exist");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess("City was saved");
   } catch (error) {
-    if(error?.response?.status == 404) {
-        printError("City not found")
+    printError(error.message);
+  }
+};
+const getForcast = async () => {
+  try {
+    const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city)
+    const response = await getWeather(city);
+    console.log(response);
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError("City not found");
     } else if (error?.response?.status == 401) {
-      printError("Invalid token")
+      printError("Invalid token");
     } else {
-      printError(error.message)
+      printError(error.message);
     }
   }
-
-}
+};
 const startCli = () => {
   const args = getArgs(process.argv);
   if (args.h) {
-    printHelp();
+    return printHelp();
   }
   if (args.s) {
+    return saveCity(args.s);
   }
   if (args.t) {
     return saveToken(args.t);
   }
-getForcast()
+  return getForcast();
 };
 
 startCli();
