b0304bfcca8fccb268123620c5465e73b2793bbf
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2025-03-17 12:28:28 +0500

    fix bugs and add axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,32 +1,28 @@
-import https from "https"
-import { TOKEN_DICTIONARY, getKeyValue } from "./storage.service.js"
+import https from "https";
+import { TOKEN_DICTIONARY, getKeyValue } from "./storage.service.js";
+import axios from "axios";
 
 const getWeather = async (city) => {
-    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
-    const token = getKeyValue(TOKEN_DICTIONARY.token)
+  const token = await getKeyValue(TOKEN_DICTIONARY.token);
 
-    if(!token) {
-        throw new Error("API doesn't exist, -t [API_KEY] for saving token")
+  if (!token) {
+    throw new Error("API doesn't exist, -t [API_KEY] for saving token");
+  }
+
+  const { data } = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      params: {
+        q: city,
+        appid: token,
+        lang: "en",
+        units: "metric",
+      },
     }
+  );
+  return data;
+};
 
-    const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-    url.searchParams.append("q", city)
-    url.searchParams.append("appid", token)
-    url.searchParams.append("lang", "en")
-    url.searchParams.append("units", "metric")
-
-    https.get(url, (response) => {
-        let res = ''
-
-        response.on('data', chunk => {
-            res += chunk
-        })
-
-        response.on("end", () => {
-            console.log(res)
-        })
-    })
-}
-
-export {getWeather}
+export { getWeather };
